ea722912702799ee2a5c3a07564d547ccf12528f
Felipe Fetter <felipetfetter@gmail.com>
2022-07-24 15:51:33 -0300

    move variables to a new file

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -28,9 +28,3 @@ resource "aws_instance" "app_server" {
     name = var.instance_name
   }
 }
-
-variable "instance_name" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string
-  default     = "ExampleAppServerInstance"
-}
